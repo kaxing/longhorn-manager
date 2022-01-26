3bee703e1f03fce05c61293bf4d5ac8c06453d98
Derek Su <derek.su@suse.com>
2022-01-26 16:25:56 +0800

    webhook: separate the conversion and admission webhook servers
    The conversion webhook should be started before the admission webhook,
    because the operations (list and get) of longhorn resources in the
    admission webhook replies on the CRD conversion.
    
    In this commit, we separate one webhook server to one conversion webhook
    and one admission webhook server to resolve the issue.
    
    Longhorn 3571
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- webhook/client/client.go
+++ webhook/client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/pkg/errors"
@@ -16,6 +17,7 @@ import (
 	"github.com/longhorn/longhorn-manager/datastore"
 	lhclientset "github.com/longhorn/longhorn-manager/k8s/pkg/client/clientset/versioned"
 	lhinformers "github.com/longhorn/longhorn-manager/k8s/pkg/client/informers/externalversions"
+	"github.com/longhorn/longhorn-manager/types"
 )
 
 type Client struct {
@@ -23,7 +25,7 @@ type Client struct {
 	Datastore *datastore.DataStore
 }
 
-func New(ctx context.Context, config *rest.Config, namespace string) (*Client, error) {
+func New(ctx context.Context, config *rest.Config, namespace, serverType string) (*Client, error) {
 	if err := schemes.Register(v1.AddToScheme); err != nil {
 		return nil, err
 	}
@@ -33,22 +35,36 @@ func New(ctx context.Context, config *rest.Config, namespace string) (*Client, e
 		return nil, err
 	}
 
-	kubeClient, err := clientset.NewForConfig(config)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to get k8s client")
-	}
+	if serverType == types.WebhookTypeAdmission {
+		kubeClient, err := clientset.NewForConfig(config)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to get k8s client")
+		}
 
-	lhClient, err := lhclientset.NewForConfig(config)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to get lh client")
-	}
+		lhClient, err := lhclientset.NewForConfig(config)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to get lh client")
+		}
 
-	kubeInformerFactory := informers.NewSharedInformerFactory(kubeClient, time.Second*30)
-	lhInformerFactory := lhinformers.NewSharedInformerFactory(lhClient, time.Second*30)
-	datastore := datastore.NewDataStore(lhInformerFactory, lhClient, kubeInformerFactory, kubeClient, namespace)
+		kubeInformerFactory := informers.NewSharedInformerFactory(kubeClient, time.Second*30)
+		lhInformerFactory := lhinformers.NewSharedInformerFactory(lhClient, time.Second*30)
+
+		datastore := datastore.NewDataStore(lhInformerFactory, lhClient, kubeInformerFactory, kubeClient, namespace)
+
+		go kubeInformerFactory.Start(ctx.Done())
+		go lhInformerFactory.Start(ctx.Done())
+
+		if !datastore.Sync(ctx.Done()) {
+			return nil, fmt.Errorf("datastore cache sync up failed")
+		}
+
+		return &Client{
+			Clients:   *clients,
+			Datastore: datastore,
+		}, nil
+	}
 
 	return &Client{
-		Clients:   *clients,
-		Datastore: datastore,
+		Clients: *clients,
 	}, nil
 }
